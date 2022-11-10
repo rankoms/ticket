29c4eb2dab02ec8dad063340e8ab18873da79ff8
rankoms <randy.komara@r17.co.id>
2022-11-10 08:02:13 +0700

    update tampilan

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,11 +1,5 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-	<!-- Brand Logo -->
-	<a href="../../index3.html" class="brand-link">
-		<img src="{{ url('adminlte') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-			style="opacity: .8">
-		<span class="brand-text font-weight-light">E-Ticket</span>
-	</a>
 
 	<!-- Sidebar -->
 	<div class="sidebar">
--- resources/views/redeem_voucher.blade.php
+++ resources/views/redeem_voucher.blade.php
@@ -53,127 +53,135 @@
 			width: 100%;
 		}
 
-	body {
-  		width: 100%;
-  		height: 120vh;
- 		background: url('images/bg.png'); center top no-repeat;
-  		background-size: cover;
-  		position: relative;
+		.swal2-popup .swal2-success-circular-line-left,
+		.swal2-popup .swal2-success-circular-line-right,
+		.swal2-popup .swal2-success-fix {
+			background-color: transparent !important;
+		}
+
+		body {
+			width: 100%;
+			height: 120vh;
+			background: url('images/bg.png');
+			center top no-repeat;
+			background-size: cover;
+			position: relative;
 		}
 	</style>
 </head>
 
 <!-- <body style="background-image:url('images/bg.png');"> -->
 
-	<form action="" id="form-voucher">
-		<div class="min-vh-100 d-flex flex-row align-items-center">
-			<div class="container">
-				<div class="row justify-content-center opacity-100">
-					<div class="col-md-6">
-						<div class="card p-4">
-							<div class="row">
+<form action="" id="form-voucher">
+	<div class="min-vh-100 d-flex flex-row align-items-center">
+		<div class="container">
+			<div class="row justify-content-center opacity-100">
+				<div class="col-md-6">
+					<div class="card p-4">
+						<div class="row">
 
-							</div>
-							<div class="clearfix">
-							<!-- <img src="images/logosabang.png" class="img-fluid" alt=""> -->
-								<h1 class="text-center">Scan E-Ticket</h1>
-								<br>
-							</div>
-							<input class="form-control mb-3" id="voucher" name="voucher" size="16" type="text"
-								placeholder="" autofocus autocomplete="false">
-							<button class="btn btn-info" style="width:100%" type="submit" autofocus="false">Search</button>
 						</div>
+						<div class="clearfix">
+							<!-- <img src="images/logosabang.png" class="img-fluid" alt=""> -->
+							<h1 class="text-center">Scan E-Ticket</h1>
+							<br>
+						</div>
+						<input class="form-control mb-3" id="voucher" name="voucher" size="16" type="text" placeholder=""
+							autofocus autocomplete="false">
+						<button class="btn btn-info" style="width:100%" type="submit" autofocus="false">Search</button>
 					</div>
 				</div>
 			</div>
-			<a href="{{ route('user.logout') }}" class="btn btn-danger position-absolute"
-				style="bottom: 10px; right:10px">Logout</a>
-			<a href="{{ route('redeem_voucher.dashboard') }}" class="btn btn-info position-absolute"
-				style="bottom: 10px; left:10px">Dashboard Redeem</a>
 		</div>
-	</form>
-	<!-- CoreUI and necessary plugins-->
-	<script src="{{ url('js/coreui.bundle.min.js') }}"></script>
-	<script src="{{ url('js/simplebar.min.js') }}"></script>
-	<script src="{{ url('js/sweetalert2@11.js') }}"></script>
-	<script type="text/javascript" src="{{ url('js/jquery.min.js') }}"></script>
+		<a href="{{ route('user.logout') }}" class="btn btn-danger position-absolute"
+			style="bottom: 10px; right:10px">Logout</a>
+		<a href="{{ route('redeem_voucher.dashboard') }}" class="btn btn-info position-absolute"
+			style="bottom: 10px; left:10px">Dashboard Redeem</a>
+	</div>
+</form>
+<!-- CoreUI and necessary plugins-->
+<script src="{{ url('js/coreui.bundle.min.js') }}"></script>
+<script src="{{ url('js/simplebar.min.js') }}"></script>
+<script src="{{ url('js/sweetalert2@11.js') }}"></script>
+<script type="text/javascript" src="{{ url('js/jquery.min.js') }}"></script>
 
-	<script>
-		$('#form-voucher').on('submit', function(e) {
-			e.preventDefault();
-			var data = getJSON("{{ route('redeem_voucher.cek_redeem_voucher') }}", {
-				_token: '{{ csrf_token() }}',
-				voucher: $('#voucher').val()
-			});
+<script>
+	$('#form-voucher').on('submit', function(e) {
+		e.preventDefault();
+		var data = getJSON("{{ route('redeem_voucher.cek_redeem_voucher') }}", {
+			_token: '{{ csrf_token() }}',
+			voucher: $('#voucher').val()
+		});
 
-			if (data.meta.code != 200) {
-				Swal.fire(
-					'Gagal',
-					data.meta.message,
-					'error'
-				)
-				Swal.fire({
-					title: 'Gagal',
-					text: data.meta.message,
-					icon: 'error',
-					showConfirmButton: false,
-					showCloseButton: true,
+		if (data.meta.code != 200) {
+			Swal.fire(
+				'Gagal',
+				data.meta.message,
+				'error'
+			)
+			Swal.fire({
+				title: 'Gagal',
+				text: data.meta.message,
+				icon: 'error',
+				showConfirmButton: false,
+				showCloseButton: true,
 
-					background: 'rgba(255,255,255,0.4)',
-					backdrop: `
+				background: 'rgba(255,255,255,0.4)',
+				backdrop: `
     rgba(0,0,123,0.4)
     url("/images/bg.png")
   `,
-					color: '#000',
-					showCloseButton: true,
-				})
+				color: '#000',
+				showCloseButton: true,
+			})
 
-			} else {
-				if (data.data.status == 1) {
+		} else {
+			if (data.data.status == 1) {
 
-					Swal.fire({
-						title: data.data.name,
-						icon: 'error',
-						html: `<p>${data.data.email}</p>
+				Swal.fire({
+					title: data.data.name,
+					icon: 'error',
+					html: `<p>${data.data.email}</p>
 								<p>${data.data.kategory}</p>
 								<p>E-Ticket Sudah di Redeem Pada ${data.data.redeem_date}</p>
 								<button disabled class="btn btn-danger">E-Ticket Sudah Di Redeem</button>
 						`,
-						showCancelButton: false,
-						showConfirmButton: false,
-						cancelButtonColor: '#d33',
-						cancelButtonText: 'Ticket Sudah Di gunakan',
-						showCloseButton: true,
-						allowOutsideClick: false,
-						background: 'rgba(255,255,255,0.4)',
-						backdrop: `
+					showCancelButton: false,
+					showConfirmButton: false,
+					cancelButtonColor: '#d33',
+					cancelButtonText: 'Ticket Sudah Di gunakan',
+					showCloseButton: true,
+					allowOutsideClick: false,
+					background: 'rgba(255,255,255,0.4)',
+					backdrop: `
     rgba(0,0,123,0.4)
     url("/images/bg2.png")
   `,
-						color: '#000'
-					}).then((result) => {
-						$('#voucher').val('');
-						$('#voucher').focus();
-						/* Read more about isConfirmed, isDenied below */
-						// window.location = "{{ route('redeem_voucher.index') }}/" + $('#voucher').val()
-					});
-				} else {
-					id = data.data.id;
-					Swal.fire({
-						title: data.data.name,
-						showCloseButton: true,
-						icon: 'success',
-						background: 'rgba(255,255,255,0.4)',
-						backdrop: `
+					color: '#000'
+				}).then((result) => {
+					$('#voucher').val('');
+					$('#voucher').focus();
+					/* Read more about isConfirmed, isDenied below */
+					// window.location = "{{ route('redeem_voucher.index') }}/" + $('#voucher').val()
+				});
+			} else {
+				id = data.data.id;
+				Swal.fire({
+					title: data.data.name,
+					showCloseButton: true,
+					icon: 'success',
+					background: 'rgba(255,255,255,0.4)',
+					backdrop: `
     rgba(0,0,123,0.4)
     url("/images/bg2.png")
   `,
-						color: '#000',
-						html: `<p>${data.data.email}</p>
+					color: '#000',
+					html: `<p>${data.data.email}</p>
 								<p>${data.data.kategory}</p>
 						`,
-						confirmButtonText: 'Tukarkan E-Ticket',
-					}).then((result) => {
+					confirmButtonText: 'Tukarkan E-Ticket',
+				}).then((result) => {
+					if (result.isConfirmed) {
 						var data = getJSON("{{ route('redeem_voucher.redeem_voucher_update') }}", {
 							_token: '{{ csrf_token() }}',
 							id: id
@@ -191,34 +199,32 @@
   `,
 							color: '#000'
 						})
-						$('#voucher').val('');
-						$('#voucher').focus();
-						// alert('oke')
-						/* Read more about isConfirmed, isDenied below */
-						// window.location = "{{ route('redeem_voucher.index') }}/" + $('#voucher').val()
-					});
-				}
+					}
+					$('#voucher').val('');
+					$('#voucher').focus();
+				});
 			}
-			$('#voucher').focus();
-			$('#voucher').val('');
-		});
-
-		$(".swal2-modal").css('background-color', '#000'); //Optional changes the color of the sweetalert 
-		$(".swal2-container.in").css('background-color', 'rgba(43, 165, 137, 0.45)'); //changes the color of the overlay
-		function getJSON(url, data, type = 'POST') {
-			return JSON.parse($.ajax({
-				type: type,
-				url: url,
-				data: data,
-				dataType: 'json',
-				global: false,
-				async: false,
-				success: function(msg) {
-
-				}
-			}).responseText);
 		}
-	</script>
+		$('#voucher').focus();
+		$('#voucher').val('');
+	});
+
+	$(".swal2-modal").css('background-color', '#000'); //Optional changes the color of the sweetalert 
+	$(".swal2-container.in").css('background-color', 'rgba(43, 165, 137, 0.45)'); //changes the color of the overlay
+	function getJSON(url, data, type = 'POST') {
+		return JSON.parse($.ajax({
+			type: type,
+			url: url,
+			data: data,
+			dataType: 'json',
+			global: false,
+			async: false,
+			success: function(msg) {
+
+			}
+		}).responseText);
+	}
+</script>
 
 </body>
 
