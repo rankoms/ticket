628dd6df97163667dcf68baad9b7274c4f965be6
rankoms <randy.komara@r17.co.id>
2023-04-03 07:52:33 +0700

    merubah column dan menambahkan column checkout by

---
+++ database/migrations/2023_04_03_075145_add_checkout_by_to_tickets.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCheckoutByToTickets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            //
+
+            $table->unsignedBigInteger('checkout_by')->index()->nullable();
+            $table->foreign('checkout_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            //
+        });
+    }
+}
