6e89740ad5ea445ef8dc8ac01f378e147de7ccca
tvqimam <tvqimam@gmail.com>
2023-09-27 09:06:41 +0700

    disable scan qr wb redeem

--- resources/views/redeem_voucher_barcode.blade.php
+++ resources/views/redeem_voucher_barcode.blade.php
@@ -439,7 +439,7 @@
                                 <input type="text" name="category" placeholder="Full Name" class="form-control" required readonly value="${data.data.kategory}" disabled>
                             </div>
                             <div class="col-12 p-0 m-0 mb-3">
-                                <label class="float-start">QR Code Wristband ssaarararr</label>
+                                <label class="float-start">QR Code Wristband</label>
                                 <input type="text" name="barcode_no" id="barcode_no" placeholder="QR Code Wristband" class="form-control" required disabled>
                             </div>
                         </div>
